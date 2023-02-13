5981f96869c6bfd5ec534c7dab7d71aa64561869
Qasim Ali <54708575+qasimlucky@users.noreply.github.com>
2023-02-13 14:04:32 +0500

    first commit

---
+++ src/mockdata/hollywood-movies.js
@@ -0,0 +1,77 @@
+export const data = [
+  {
+    id: 1,
+    thumbnail:"https://lh6.googleusercontent.com/H_m77ZMqrhDN3Mvdh3UCGsyNTRahjpdl5YgIn94XA-1N214YZsYGmgdxKp8cREr43VY=w2400",
+    video:"https://drive.google.com/file/d/1Z33KOxLan_dWA0ZG3imfxt4bscBd3927/preview",
+  },
+  {
+    id: 2,
+    thumbnail:"https://lh6.googleusercontent.com/jUhyhLqxHaR_ZjdAu0tBwlJBn1Z0C6fVBJBm87jhTbzNZglOuRGCTlX-5oXh-X21PxU=w2400",
+    video:"https://drive.google.com/file/d/18GninB6LYHXCpCVw-7OYOybwQgrW-EWa/preview",
+  },
+  {
+    id: 3,
+    thumbnail:"https://lh4.googleusercontent.com/Nirfgy9POfx7t4do1NEgaud4CqLWQJMKfXFlWMUcnuZjdWJMmHpo56xU3h4jt3v5e2E=w2400",
+    video:"https://drive.google.com/file/d/1uvHKgVXh7TFqjJSrRou9CfkhvyKqbs6W/preview",
+  },
+  {
+    id: 4,
+    thumbnail:"https://drive.google.com/file/d/1JfQvxNiwQESAPQyJM4O8X74lGYMG-BNv/view?usp=sharing",
+    video:"https://drive.google.com/file/d/1yPeKNq5pSDlfgMrTFD4jqaldcWrCLLEj/preview",
+  },
+  {
+    id: 5,
+    thumbnail:"https://lh3.googleusercontent.com/QyPof9CkbUfA-iDrnLieKhKCnxdRaWWEC9z2ARhviJwU7snUS2RRPYUOm2CSh0xfoug=w2400",
+    video:"https://drive.google.com/file/d/17gc6SFkb5hIlw2TUE0Zv-dZLCmOhqHLl/preview",
+  },
+  {
+    id: 6,
+    thumbnail:"https://lh4.googleusercontent.com/h9vSNBPjyWAbXQnMuBFRZBc9JwM-_Y5CxpCGvx7L3nozVpN6JZ8p9-SJM2tlrcq3k-s=w2400",
+    video:"https://drive.google.com/file/d/19zSd0r24IZdbloE-_vlor3YTt7R6Rqdw/preview",
+  },
+  {
+    id: 7,
+    thumbnail:"https://drive.google.com/file/d/1JBimnUCkBsC6zJPpgCrj7sAMDxaUqGQ4/view?usp=sharing",
+    video:"https://drive.google.com/file/d/1j0vkE_MO7V3nvGx9zynkJXJ2V-EHguj1/preview",
+  },
+  {
+    id: 8,
+    thumbnail:"https://drive.google.com/file/d/1W0V0ygl831AIA3fgpr8HRTAlTs1VpU9v/view?usp=sharing",
+    video:"https://drive.google.com/file/d/1Ww4z3UIANK4n1Lfb-3DhDZ9egUQttutA/preview",
+  },
+  {
+    id: 9,
+    thumbnail:"https://lh5.googleusercontent.com/jHU3ikcxuFZkyU_GTne96GSI01RUHh2EWGgkYyhQC_eEBQtF9zPc8UhxDRpff00aIYQ=w2400",
+    video:"https://drive.google.com/file/d/1XSxzp1P-06pbPCh76a97RlF2doYqpuoS/preview",
+  },
+  {
+    id: 10,
+    thumbnail:"https://lh5.googleusercontent.com/eJmI3X1HT5oqz4ze-74RGe3Hqg3QCfU7ZG4eeywfRPfc91hjO6ah4e2wnlmhgRdADQQ=w2400",
+    video:"https://drive.google.com/file/d/1KMJrfV8xWMm_L1KMbHKN_Nnu7dem_DtD/preview",
+  },
+  {
+    id: 11,
+    thumbnail:"https://lh3.googleusercontent.com/OpbeL3qu5mcoeWsFMb2Jg5ppOm0xs2j4S1veTZVNDpykdiezdOKFH2DnHUQaL8H8Lzw=w2400",
+    video:"https://drive.google.com/file/d/1qUjLlGYBjYp-Un4h5nHo17LTrhvWpo9M/preview",
+  },
+  {
+    id: 12,
+    thumbnail:"https://lh5.googleusercontent.com/zjOoKVeRcfhpFsgjgcZcbEuWKBZJQClExudftbFaOchxBQKwI42uuT7EpahFvHOOI3A=w2400",
+    video:"https://drive.google.com/file/d/11074izpVNISUM9StsX21EVNLlykLwhi0/preview",
+  },
+  {
+    id: 13,
+    thumbnail:"https://lh4.googleusercontent.com/dNEAUGzaIq8T6ON5kIGeYHtIMmoiMApxOkGhZpQnEFueMOP36cI8IeQ4crRgU36PYrE=w2400",
+    video:"https://drive.google.com/file/d/1NE2kzZlSIcn-f7vmSonxtaZIyqHrzhJD/preview",
+  },
+  {
+    id: 14,
+    thumbnail:"https://lh3.googleusercontent.com/HB33hNjUwk1QnUcoxeuM_FOy7TAJcW60GpNaczkMrJwrme8k3h2J1Db-ffzAaTyHUiM=w2400",
+    video:"https://drive.google.com/file/d/12x_wou5948ALPVoBUFiIXy88sh4zSC3E/preview",
+  },
+  {
+    id: 15,
+    thumbnail:"https://lh6.googleusercontent.com/rVWqwlCWK_kguwDYdqWfq8cTM0ucNQKBFWKmCerDxqHluP29Ky1YMeERUAVDICMuUrY=w2400",
+    video:"https://drive.google.com/file/d/1AZhU0uExTUSJGK3NSEbOwFUq8x_n47TW/preview",
+  },
+];
--- src/mockdata/mockDataB.js
+++ src/mockdata/mockDataB.js
@@ -1,4 +1,4 @@
-export const dataB = [
+/* export const dataB = [
     {
       id: 1,
       img: 'https://lh5.googleusercontent.com/uvZnP4GBkEgSqaYaiUymBlfGyopSQWmycYTUz7SE5URPqerT_L7NkhQ-49WR044Cju8=w2400',
@@ -83,5 +83,107 @@ export const dataB = [
       id: 21,
       img: 'https://lh5.googleusercontent.com/b4j1fRzFt3907j-un2OUNBOeTtGFE_TPw2Aob-d_ONOdkUfov91z9AdHrPY97fqiHfk=w2400',
     },
-  ];
+  ]; */
   
+  export const dataB = [
+    {
+      id: 1,
+	    img:"https://lh6.googleusercontent.com/H_m77ZMqrhDN3Mvdh3UCGsyNTRahjpdl5YgIn94XA-1N214YZsYGmgdxKp8cREr43VY=w2400",
+	    video:"https://drive.google.com/file/d/1Z33KOxLan_dWA0ZG3imfxt4bscBd3927/preview",
+    },
+    {
+      id: 2,
+     	img:"https://lh6.googleusercontent.com/jUhyhLqxHaR_ZjdAu0tBwlJBn1Z0C6fVBJBm87jhTbzNZglOuRGCTlX-5oXh-X21PxU=w2400",
+	    video:"https://drive.google.com/file/d/18GninB6LYHXCpCVw-7OYOybwQgrW-EWa/preview",
+    },
+    {
+      id: 3,
+      	img:"https://lh4.googleusercontent.com/Nirfgy9POfx7t4do1NEgaud4CqLWQJMKfXFlWMUcnuZjdWJMmHpo56xU3h4jt3v5e2E=w2400",
+	      video:"https://drive.google.com/file/d/1uvHKgVXh7TFqjJSrRou9CfkhvyKqbs6W/preview",
+    },
+    {
+      id: 4,
+        img:"https://drive.google.com/file/d/1JfQvxNiwQESAPQyJM4O8X74lGYMG-BNv/view?usp=sharing",
+	      video:"https://drive.google.com/file/d/1yPeKNq5pSDlfgMrTFD4jqaldcWrCLLEj/preview",
+    },
+    {
+      id: 5,
+      	img:"https://lh3.googleusercontent.com/QyPof9CkbUfA-iDrnLieKhKCnxdRaWWEC9z2ARhviJwU7snUS2RRPYUOm2CSh0xfoug=w2400",
+	      video:"https://drive.google.com/file/d/17gc6SFkb5hIlw2TUE0Zv-dZLCmOhqHLl/preview",
+    },
+    {
+      id: 6,
+     	img:"https://lh4.googleusercontent.com/h9vSNBPjyWAbXQnMuBFRZBc9JwM-_Y5CxpCGvx7L3nozVpN6JZ8p9-SJM2tlrcq3k-s=w2400",
+	    video:"https://drive.google.com/file/d/19zSd0r24IZdbloE-_vlor3YTt7R6Rqdw/preview",
+    },
+    {
+      id: 7,
+      	img:"https://drive.google.com/file/d/1JBimnUCkBsC6zJPpgCrj7sAMDxaUqGQ4/view?usp=sharing",
+	      video:"https://drive.google.com/file/d/1j0vkE_MO7V3nvGx9zynkJXJ2V-EHguj1/preview",
+    },
+    {
+      id: 8,
+       img:"https://drive.google.com/file/d/1W0V0ygl831AIA3fgpr8HRTAlTs1VpU9v/view?usp=sharing",
+	      video:"https://drive.google.com/file/d/1Ww4z3UIANK4n1Lfb-3DhDZ9egUQttutA/preview",
+    },
+    {
+      id: 9,
+      	img:"https://lh5.googleusercontent.com/jHU3ikcxuFZkyU_GTne96GSI01RUHh2EWGgkYyhQC_eEBQtF9zPc8UhxDRpff00aIYQ=w2400",
+	      video:"https://drive.google.com/file/d/1XSxzp1P-06pbPCh76a97RlF2doYqpuoS/preview",
+    },
+    {
+      id: 10,
+     	img:"https://lh5.googleusercontent.com/eJmI3X1HT5oqz4ze-74RGe3Hqg3QCfU7ZG4eeywfRPfc91hjO6ah4e2wnlmhgRdADQQ=w2400",
+	    video:"https://drive.google.com/file/d/1KMJrfV8xWMm_L1KMbHKN_Nnu7dem_DtD/preview",
+    },
+    {
+      id: 11,
+     	img:"https://lh3.googleusercontent.com/OpbeL3qu5mcoeWsFMb2Jg5ppOm0xs2j4S1veTZVNDpykdiezdOKFH2DnHUQaL8H8Lzw=w2400",
+	    video:"https://drive.google.com/file/d/1qUjLlGYBjYp-Un4h5nHo17LTrhvWpo9M/preview",
+    },
+    {
+      id: 12,
+     	img:"https://lh5.googleusercontent.com/zjOoKVeRcfhpFsgjgcZcbEuWKBZJQClExudftbFaOchxBQKwI42uuT7EpahFvHOOI3A=w2400",
+	    video:"https://drive.google.com/file/d/11074izpVNISUM9StsX21EVNLlykLwhi0/preview",
+    },
+    {
+      id: 13,
+     	img:"https://lh4.googleusercontent.com/dNEAUGzaIq8T6ON5kIGeYHtIMmoiMApxOkGhZpQnEFueMOP36cI8IeQ4crRgU36PYrE=w2400",
+	    video:"https://drive.google.com/file/d/1NE2kzZlSIcn-f7vmSonxtaZIyqHrzhJD/preview",
+    },
+    {
+      id: 14,
+     	img:"https://lh3.googleusercontent.com/HB33hNjUwk1QnUcoxeuM_FOy7TAJcW60GpNaczkMrJwrme8k3h2J1Db-ffzAaTyHUiM=w2400",
+	    video:"https://drive.google.com/file/d/12x_wou5948ALPVoBUFiIXy88sh4zSC3E/preview",
+    },
+    {
+      id: 15,
+      	img:"https://lh6.googleusercontent.com/rVWqwlCWK_kguwDYdqWfq8cTM0ucNQKBFWKmCerDxqHluP29Ky1YMeERUAVDICMuUrY=w2400",
+	      video:"https://drive.google.com/file/d/1AZhU0uExTUSJGK3NSEbOwFUq8x_n47TW/preview",
+    },
+    {
+      id: 16,
+      img: 'https://wallpaperaccess.com/full/1923020.jpg',
+    },
+    // second
+    {
+      id: 17,
+      img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQubAUOPoCA0tRK7-t7e0hCujgTHzBpZFK0Aw&usqp=CAU',
+    },
+    {
+      id: 18,
+      img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQUyoyX09MPGJfMcO9DkZxytToD8lx6Td9T5g&usqp=CAU',
+    },
+    {
+      id: 19,
+      img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQsrxCy0ymSPJyC7ZgNHkNRIcdPztvMD8tGkg&usqp=CAU',
+    },
+    {
+      id: 20,
+      img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQsrxCy0ymSPJyC7ZgNHkNRIcdPztvMD8tGkg&usqp=CAU',
+    },
+    {
+      id: 21,
+      img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQubAUOPoCA0tRK7-t7e0hCujgTHzBpZFK0Aw&usqp=CAU',
+    },
+  ];
